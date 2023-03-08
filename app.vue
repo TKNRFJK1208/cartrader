e279ee6e12baf8f443895295f2ccc64ef4e357d6
takanori <takanori@goopter.com>
2023-03-07 22:03:27 -0800

    installed tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- navbar -->
+    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+      <a class="text-3xl font-0mono" href="">cartrader</a>
+    </header>
   </div>
 </template>
