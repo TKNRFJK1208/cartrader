278a552ca29d37584b6f29ae23213085d5020796
takanori <takanori@goopter.com>
2023-03-18 12:18:53 -0700

    added Server side error handling

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,10 +1,23 @@
 <script setup>
 const route = useRoute();
+const { cars } = useCars();
 const { toTitleCase } = useUtilities();
-
 useHead({
   title: toTitleCase(route.params.name),
 })
+
+const car = computed(() => {
+  return cars.find((c) => {
+    return c.id === parseInt(route.params.id);
+  })
+})
+
+if (!car.value) {
+  throw createError({
+    statusCode: 404,
+    message: `Car with id of ${route.params.id} did not exist`
+  })
+}
 
 // layout/custom.vue
 definePageMeta({
@@ -14,10 +27,10 @@ definePageMeta({
 </script>
 
 <template>
-  <div>
-    <CarDetailHero />
-    <CarDetailAttributes />
-    <CarDetailDescription />
+  <div v-if="car">
+    <CarDetailHero :car="car" />
+    <CarDetailAttributes :features="car.features" />
+    <CarDetailDescription :description="car.description" />
     <CarDetailContact />
   </div>
 </template>
